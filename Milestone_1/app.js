98975e81f66286ab4e1f8e295ab155fd5f1791cf
Rune Rask <r.rask@hotmail.com>
2018-09-13 14:30:57 +0200

    Fix

--- Milestone_1/app.js
+++ Milestone_1/app.js
@@ -39,7 +39,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-console.log(os.networkInterfaces.ip);
+console.log('PI IP: ' + os.networkInterfaces.ip);
 
 console.log('This server is running at ' + ip.address() + ':3000');
 
